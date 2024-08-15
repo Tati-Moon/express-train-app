c581907e9c232eea07a37373bc775f31c002cde0
Aleksandr.Dmitriev <dmitan@bk.ru>
2024-08-15 23:22:02 +0300

    chore: 01 update version angular to 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
+import { AppEffects } from './redux/effects/app.effects';
+import { metaReducers, reducers } from './redux/reducers';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes)],
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideAnimationsAsync(),
+        provideRouter(routes),
+        provideHttpClient(withInterceptors([])),
+        provideStore(reducers, { metaReducers }),
+        provideEffects(AppEffects),
+        provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    ],
 };
