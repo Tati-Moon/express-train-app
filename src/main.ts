105ab083a830c118cf648ccac0eafa387c6f04f1
Aleksandr.Dmitriev <dmitan@bk.ru>
2024-08-15 22:39:26 +0300

    chrore: 01 update configs eslint and prettier

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,4 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-const test = 1;
-console.log('🚀 ~ test:', test);
-
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig);
